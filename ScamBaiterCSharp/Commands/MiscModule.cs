d9515b6959471046e89e62cf9007695247eda233
Scott Gould <greysilly7@gmail.com>
2023-01-28 16:23:02 -0500

    4 space tabs ;()

--- ScamBaiterCSharp/Commands/MiscModule.cs
+++ ScamBaiterCSharp/Commands/MiscModule.cs
@@ -8,28 +8,28 @@ namespace ScamBaiterCSharp.Commands;
 
 public class MiscModule : BaseCommandModule
 {
-    [Command("update_db"), RequireOwner]
-    public async Task UpdateDbCommand(CommandContext ctx)
-    {
-        MiscUtils.UpdateScamDatabase();
-        MiscUtils.UpdateServerDatabase();
+  [Command("update_db"), RequireOwner]
+  public async Task UpdateDbCommand(CommandContext ctx)
+  {
+    MiscUtils.UpdateScamDatabase();
+    MiscUtils.UpdateServerDatabase();
 
-        await ctx.RespondAsync("Updated Databases");
-    }
+    await ctx.RespondAsync("Updated Databases");
+  }
 
-    [Command("botinfo")]
-    public async Task BotInfoCommand(CommandContext ctx)
-    {
-        var embed = new DiscordEmbedBuilder()
-            .WithTitle("Bot Information")
-            .WithTimestamp(DateTime.Now)
-            .AddField("System Information",
-                // I am incapable of doing the Memory stuff unless we go linux only;( cross platform made me wanna kms
-                $"Hostname: {Dns.GetHostName()}\nTotal Memory: N/A\n Free Memory: N/A")
-            .AddField("Bot Info",
-                $"Bot name: {ctx.Client.CurrentUser.Username}\nGuild Count: {ctx.Client.Guilds.Count()}")
-            .Build();
+  [Command("botinfo")]
+  public async Task BotInfoCommand(CommandContext ctx)
+  {
+    var embed = new DiscordEmbedBuilder()
+        .WithTitle("Bot Information")
+        .WithTimestamp(DateTime.Now)
+        .AddField("System Information",
+            // I am incapable of doing the Memory stuff unless we go linux only;( cross platform made me wanna kms
+            $"Hostname: {Dns.GetHostName()}\nTotal Memory: N/A\n Free Memory: N/A")
+        .AddField("Bot Info",
+            $"Bot name: {ctx.Client.CurrentUser.Username}\nGuild Count: {ctx.Client.Guilds.Count()}")
+        .Build();
 
-        await ctx.RespondAsync(embed);
-    }
+    await ctx.RespondAsync(embed);
+  }
 }
--- ScamBaiterCSharp/Util/Misc.cs
+++ ScamBaiterCSharp/Util/Misc.cs
@@ -5,38 +5,38 @@ namespace ScamBaiterCSharp.Util;
 
 public class MiscUtils
 {
-    public static async void UpdateScamDatabase()
+  public static async void UpdateScamDatabase()
+  {
+    Console.WriteLine("[INFO] Updating Scam DB");
+    var client = new HttpClient();
+    client.BaseAddress = new Uri("https://phish.sinking.yachts");
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ScamBaiter", "1.0"));
+    ;
+    var response = await client.GetAsync("/v2/all");
+
+    if (response.IsSuccessStatusCode)
     {
-        Console.WriteLine("[INFO] Updating Scam DB");
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("https://phish.sinking.yachts");
-        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ScamBaiter", "1.0"));
-        ;
-        var response = await client.GetAsync("/v2/all");
-
-        if (response.IsSuccessStatusCode)
-        {
-            var content = await response.Content.ReadAsStringAsync();
-            await File.WriteAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "scamdb.json"), content);
-        }
+      var content = await response.Content.ReadAsStringAsync();
+      await File.WriteAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "scamdb.json"), content);
     }
+  }
 
-    public static async void UpdateServerDatabase()
+  public static async void UpdateServerDatabase()
+  {
+    Console.WriteLine("[INFO] Updating Server DB");
+
+    var client = new HttpClient();
+    client.BaseAddress = new Uri("https://api.phish.gg");
+    client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ScamBaiter", "1.0"));
+    ;
+    var response = await client.GetAsync("/servers/all");
+
+    if (response.IsSuccessStatusCode)
     {
-        Console.WriteLine("[INFO] Updating Server DB");
-
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("https://api.phish.gg");
-        client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("ScamBaiter", "1.0"));
-        ;
-        var response = await client.GetAsync("/servers/all");
-
-        if (response.IsSuccessStatusCode)
-        {
-            var content = await response.Content.ReadAsStringAsync();
-            await File.WriteAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "serverdb.json"), content);
-        }
+      var content = await response.Content.ReadAsStringAsync();
+      await File.WriteAllTextAsync(Path.Combine(Directory.GetCurrentDirectory(), "serverdb.json"), content);
     }
+  }
 
 
 }
